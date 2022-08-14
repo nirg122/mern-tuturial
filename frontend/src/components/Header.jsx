ec569dc97133eefc78a14cdd9ed825e56a97c739
Nir <nirg122@gmail.com>
2022-08-14 12:24:44 +0300

    Added frontend UI

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt>Login</FaSignInAlt>
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUser>Register</FaUser>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
